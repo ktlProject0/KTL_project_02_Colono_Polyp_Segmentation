2030af946faafea8e0a7204a8e06ae9f05a0bd94
ktlProject0 <ktlproject0@gmail.com>
2024-10-25 15:39:47 +0900

    commit message

---
+++ dataset.py
@@ -0,0 +1,75 @@
+import os
+import glob
+from natsort import natsorted
+import numpy as np
+import pandas as pd
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from torch.utils.data import Dataset
+from torchvision.datasets.folder import default_loader
+
+class CustomDataset(Dataset):
+    def __init__(self,direc,mode='eval'):
+        
+        img_path = natsorted(glob.glob(os.path.join(direc,'images','*')))
+        mask_path = natsorted(glob.glob(os.path.join(direc,'masks','*')))
+        self.meta_df = pd.DataFrame({"image":img_path,'label':mask_path})
+        if mode =='train':
+            self.transform = A.Compose([
+                    A.Resize(width=224, height=224),
+                    A.HorizontalFlip(),
+                    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+                    A.ShiftScaleRotate(shift_limit=(-0.05,0.05),scale_limit=(-0.1,0.2),rotate_limit=(-10,10),p=0.5),
+                    A.Normalize(mean=0.5, 
+                                std=0.5, 
+                                max_pixel_value=1.0, always_apply=True, p=1.0),
+                    ToTensorV2(transpose_mask=True)
+                ])
+        elif mode=='eval':
+            self.transform = A.Compose([
+    A.Resize(width=224, height=224),
+    A.Normalize(mean=0.5, 
+                std=0.5, 
+                max_pixel_value=1.0, always_apply=True, p=1.0),
+    ToTensorV2(transpose_mask=True)
+])
+        
+        self.cache={}
+        
+    def __len__(self):
+        return len(self.meta_df)
+
+    
+    def __getitem__(self,idx):
+        if idx in self.cache:
+            sample = self.cache[idx]
+        else:
+            sample = self.meta_df.iloc[idx,:].to_dict()
+            
+            image = (np.array(default_loader(sample['image']))/255.).astype(np.float32)
+            mask = np.array(default_loader(sample['label']))[...,0]
+            mask = mask>100
+            mask = np.stack([(mask==x).astype(np.uint8) for x in [0,1]], axis=-1)
+
+            sample['image'] = image
+            sample['mask'] = mask
+            sample['origin_shape'] = image.shape
+
+            self.cache[idx] = sample
+            
+        if self.transform:
+            transformed = self.transform(image= sample['image'], mask = sample['mask'])
+
+        sample_input = {}
+        sample_input['input'] = transformed['image']
+        sample_input['target'] = transformed['mask']
+        sample_input['origin_shape'] = sample['origin_shape']
+        
+        return sample_input
+        
+if __name__ == '__main__':
+    train = CustomDataset('/home/sujeong/sujeong/project/KTL/2_stomach/data/','train')
+    test = CustomDataset('/home/sujeong/sujeong/project/KTL/2_stomach/data/','test')
+    for sample_input in train:
+        print(sample_input['input'].shape)
+        print(sample_input['target'].shape)
---
+++ visualize_sample.py
@@ -0,0 +1,71 @@
+import os
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+from model import Net
+from torchvision.datasets.folder import default_loader
+
+import cv2
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='Poly Segmentation')
+    parser.add_argument('--data_path', type=str,default='./data/train/images/cju0s2a9ekvms080138tjjpxr.jpg', help="data path")
+    parser.add_argument('--n_classes', type=int,default=2, help="num of classes")
+    parser.add_argument('--cuda', action='store_true', help='use cuda?')
+    parser.add_argument('--seed', type=int, default=42, help='random seed to use. Default=123')
+    parser.add_argument('--model_save_path', type=str, default='./checkpoints', help='Path for save best model')
+    opt = parser.parse_args()
+    
+    if not os.path.isdir(opt.model_save_path):
+        raise Exception("checkpoints not found, please run train.py first")
+
+    os.makedirs("Visualize_result",exist_ok=True)
+    
+    if opt.cuda and not torch.cuda.is_available():
+        raise Exception("No GPU found, please run without --cuda")
+    
+    torch.manual_seed(opt.seed)
+    
+    if opt.cuda:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    
+    device ='cuda'
+    
+    model = Net(n_classes=opt.n_classes).to(device)
+    model.load_state_dict(torch.load(os.path.join(opt.model_save_path,'model_statedict.pth'),map_location=device))
+    model.eval()
+
+    transform = A.Compose([
+        A.Resize(width=224, height=224),
+        A.Normalize(mean=0.5,std=0.5,max_pixel_value=1.0, always_apply=True, p=1.0),ToTensorV2(transpose_mask=True)])
+
+    
+    image = (np.array(default_loader(opt.data_path))/255.).astype(np.float32)
+    sample_origin_shape = image.shape
+    transformed = transform(image= image)
+    sample_input = transformed['image']
+
+    with torch.no_grad():
+        pred_logit = model(sample_input[None].to(device))
+        pred = pred_logit.argmax(dim=1).squeeze().detach().cpu()
+    origin_size_pred = torch.nn.functional.interpolate(pred[None,None].float(),size=sample_origin_shape[:2],mode='nearest').squeeze().numpy()
+
+    cbct = sample_input.clone()
+    cbct_visualize = cbct.permute(1,2,0).squeeze().detach().cpu().clone()
+    cbct_visualize = ((cbct_visualize *0.5 +0.5) *255).numpy().astype(np.uint8)
+    cbct_visualize_contour = cbct_visualize.copy()
+    contours, hier = cv2.findContours(pred.numpy().astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(cbct_visualize_contour, contours, 0, (0,0,255), 2, cv2.LINE_8, hier)
+
+    cv2.imwrite(f"Visualize_result/{opt.data_path.split('/')[-1]}",cbct_visualize[...,::-1])
+    cv2.imwrite(f"Visualize_result/{opt.data_path.split('/')[-1].split('.')[0]}_visualize.{opt.data_path.split('/')[-1].split('.')[-1]}",cbct_visualize_contour[...,::-1])
+
+    
