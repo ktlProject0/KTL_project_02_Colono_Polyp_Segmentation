4942e163a5a5e3f9f64a955545c20bd2285bd8e2
ktlProject0 <ktlproject0@gmail.com>
2024-11-18 19:46:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,35 +3,32 @@ import os
 import argparse
 import json
 from tqdm import tqdm
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from model import Net
 from dataset import CustomDataset
-from loss import DiceChannelLoss
-
+from loss import DiceChannelLoss, BCELoss
+from sklearn.metrics import precision_score, recall_score
 
 if __name__ == '__main__':
-    # Training settings
-    parser = argparse.ArgumentParser(description='Pneumothrox Segmentation')
-    parser.add_argument('--data_direc', type=str,default='./data', help="data directory")
-    parser.add_argument('--n_classes', type=int,default=1, help="num of classes")
-    parser.add_argument('--cuda', action='store_true', help='use cuda?')
-    parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
-    parser.add_argument('--seed', type=int, default=42, help='random seed to use. Default=123')
-    parser.add_argument('--testBatchSize', type=int, default=4, help='test batch size')
-    parser.add_argument('--model_save_path', type=str, default='./checkpoints', help='Path for save best model')
+    parser = argparse.ArgumentParser(description='Thyroid Segmentation')
+    parser.add_argument('--data_direc', type=str, default='./data', help="Data directory")
+    parser.add_argument('--n_classes', type=int, default=1, help="Number of classes")
+    parser.add_argument('--cuda', action='store_true', help='Use CUDA')
+    parser.add_argument('--threads', type=int, default=4, help='Number of threads for data loader')
+    parser.add_argument('--seed', type=int, default=42, help='Random seed')
+    parser.add_argument('--testBatchSize', type=int, default=1, help='Test batch size')
+    parser.add_argument('--model_save_path', type=str, default='./checkpoints', help='Path to load the model')
     opt = parser.parse_args()
     
     if not os.path.isdir(opt.model_save_path):
-        raise Exception("checkpoints not found, please run train.py first")
+        raise Exception("Checkpoints not found, please run train.py first")
 
-    os.makedirs("test_results",exist_ok=True)
+    os.makedirs("test_results", exist_ok=True)
     
     print(opt)
     
@@ -39,68 +36,78 @@ if __name__ == '__main__':
         raise Exception("No GPU found, please run without --cuda")
     
     torch.manual_seed(opt.seed)
+    device = 'cuda' if opt.cuda and torch.cuda.is_available() else 'cpu'
     
-    if opt.cuda:
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    device ='cuda'
     print('===> Loading datasets')
-    
-    test_set = CustomDataset(f"{opt.data_direc}/test",mode='eval')
+    test_set = CustomDataset(f"{opt.data_direc}/test", mode='eval')
     test_dataloader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
     
     print('===> Building model')
     model = Net(n_classes=opt.n_classes).to(device)
-    model.load_state_dict(torch.load(os.path.join(opt.model_save_path,'model_statedict.pth'),map_location=device))
+    model.load_state_dict(torch.load(os.path.join(opt.model_save_path, 'model_statedict.pth'), map_location=device))
     model.eval()
 
-    with open(os.path.join(opt.model_save_path,'metric_logger.json'), 'r') as f:
+    with open(os.path.join(opt.model_save_path, 'metric_logger.json'), 'r') as f:
         metric_logger = json.load(f)
     
-    criterion = nn.BCELoss()
-    criterion_dice =DiceChannelLoss()
-    
+    criterion = BCELoss()
+    criterion_dice = DiceChannelLoss()
     
     total_test_num = len(test_dataloader.sampler)
-    test_dice,test_ce =torch.zeros(opt.n_classes),0
+    all_dice_scores = []
+    all_precisions = []
+    all_recalls = []
+    all_ce_losses = []
     
     with torch.no_grad():
-        for data in tqdm(test_dataloader,total=len(test_dataloader),position=0,desc='Test',colour='green'):
+        for data in tqdm(test_dataloader, total=len(test_dataloader), desc='Test', colour='green'):
             batch_num = len(data['input'])
-        
-            image = data['input'].to(device)
-            target = data['target'].to(device)
+            image, target = data['input'].to(device), data['target'].to(device)
 
             pred_logit = model(image.float())
             pred = (pred_logit > 0.5).float()
-            ce_loss = criterion(pred,target.float())
-            dice_channel_loss, dice_loss = criterion_dice(pred,target)
             
-            test_dice+=dice_channel_loss.cpu()*batch_num
-            test_ce+=ce_loss.item()*batch_num
+            ce_loss = criterion(pred, target.float())
+            dice_channel_loss, _ = criterion_dice(pred, target)
+            
+            # Save individual scores
+            all_ce_losses.append(ce_loss.item())
+            all_dice_scores.append(1 - dice_channel_loss.cpu().numpy())  # Dice score = 1 - Dice loss
+
+            # Flatten predictions and targets for precision and recall
+            pred_flat = pred.cpu().numpy().flatten()
+            target_flat = target.cpu().numpy().flatten()
+            precision = precision_score(target_flat, pred_flat, average='binary', zero_division=1)
+            recall = recall_score(target_flat, pred_flat, average='binary', zero_division=1)
+            all_precisions.append(precision)
+            all_recalls.append(recall)
     
-    test_dice/=total_test_num
-    test_ce/=total_test_num
+    # Compute metrics
+    avg_dice = np.mean(all_dice_scores)
+    std_dice = np.std(all_dice_scores)
+    avg_precision = np.mean(all_precisions)
+    std_precision = np.std(all_precisions)
+    avg_recall = np.mean(all_recalls)
+    std_recall = np.std(all_recalls)
+    avg_ce_loss = np.mean(all_ce_losses)
 
+    # Save metrics
+    eval_df = pd.DataFrame({
+        "Test Dice Coefficient Score": [avg_dice],
+        "Dice Std": [std_dice],
+        "Test Precision": [avg_precision],
+        "Precision Std": [std_precision],
+        "Test Recall": [avg_recall],
+        "Recall Std": [std_recall],
+        "Binary Cross Entropy Loss": [avg_ce_loss]
+    })
+    eval_df.to_csv("test_results/metric_df.csv", index=None)
 
-    eval_df = pd.DataFrame({"Test Dice Coefficient Score":[test_dice.numpy()]})
-
-    eval_df.to_csv(f"test_results/metric_df.csv",index=None)
-
+    # Plot learning graph
     plt.figure()
-    for k in ['train_dice','val_dice']:
-        plt.plot(np.arange(len(metric_logger[k])),metric_logger[k],label=k)
+    for k in ['train_loss', 'val_loss']:
+        plt.plot(np.arange(len(metric_logger[k])), metric_logger[k], label=k)
     plt.title("Dice Coefficient Loss")
     plt.legend()
     plt.grid()
-    plt.savefig(f"test_results/learning_graph_dice_coefficient.png",dpi=200)
-    
-    
-    plt.figure()
-    for k in ['train_ce','val_ce']:
-        plt.plot(np.arange(len(metric_logger[k])),metric_logger[k],label=k)
-    plt.title("Binary Cross Entropy Loss")
-    plt.legend()
-    plt.grid()
-    plt.savefig(f"test_results/learning_graph_binary_cross_enropy.png",dpi=200)
+    plt.savefig("test_results/learning_graph_dice_coefficient.png", dpi=200)
